0f550377ef1e8d799794b3bb42343366bf370314
dane <danerieber@gmail.com>
2024-05-31 19:15:16 -0500

    UI and functionality for editing messages

--- components/ChatMessageElement.tsx
+++ components/ChatMessageElement.tsx
@@ -7,9 +7,12 @@ import remarkBreaks from "remark-breaks";
 import remarkGfm from "remark-gfm";
 import Username from "./Username";
 import remarkGemoji from "remark-gemoji";
-import { Verified } from "@mui/icons-material";
+import { Edit, Verified } from "@mui/icons-material";
 import { Tooltip } from "@nextui-org/tooltip";
 import rehypeExternalLinks from "rehype-external-links";
+import { useEffect, useRef, useState } from "react";
+import { Button, ButtonGroup } from "@nextui-org/button";
+import { Textarea } from "@nextui-org/input";
 
 export default function ChatMessageElement({
   icon,
@@ -21,6 +24,8 @@ export default function ChatMessageElement({
   isDeveloper,
   setImageModalSrc,
   imageOnOpen,
+  isOwned,
+  editChatMessage,
 }: {
   icon: string;
   username: string;
@@ -31,8 +36,49 @@ export default function ChatMessageElement({
   isDeveloper: boolean;
   setImageModalSrc: (src: string | undefined) => void;
   imageOnOpen: () => void;
+  isOwned: boolean;
+  editChatMessage: (content: string) => void;
 }) {
-  function formatTimestamp(timestamp: string) {
+  const [isHovering, setIsHovering] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedContent, setEditedContent] = useState("");
+
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+  useEffect(() => {
+    setEditedContent(msg.data.content);
+  }, [msg.data.content]);
+
+  useEffect(() => {
+    if (isEditing) {
+      textareaRef.current?.focus();
+      textareaRef.current?.setSelectionRange(
+        editedContent.length,
+        editedContent.length,
+      );
+    }
+  }, [editedContent.length, isEditing]);
+
+  function editMessage() {
+    if (editedContent !== msg.data.content) {
+      editChatMessage(editedContent);
+    }
+    setIsEditing(false);
+    setIsHovering(false);
+    setEditedContent(msg.data.content);
+  }
+
+  function handleKeyDown(e: React.KeyboardEvent<HTMLDivElement>) {
+    if (e.code === "Enter" && !e.shiftKey) {
+      e.preventDefault();
+      editMessage();
+    } else if (e.code === "Escape") {
+      e.preventDefault();
+      editMessage();
+    }
+  }
+
+  function formatTimestamp(timestamp: number) {
     const tsFormatOtherYear = "D MMM Y [at] h:mm a";
     const tsFormat = "D MMM [at] h:mm a";
     const m = moment(timestamp);
@@ -59,54 +105,103 @@ export default function ChatMessageElement({
   return (
     <div
       className={clsx(
-        "flex w-full max-w-7xl px-2 xl:px-10",
-        isMentioned() && "bg-mentioned",
+        "relative w-full max-w-7xl px-2 xl:px-10",
+        isHovering && "bg-content4",
+        isMentioned() && (isHovering ? "bg-mentioned-600" : "bg-mentioned"),
         showUsername && "mt-3",
       )}
+      onMouseEnter={() => setIsHovering(true)}
+      onMouseLeave={() => setIsHovering(false)}
     >
-      {showUsername && <div className="pr-2 w-5">{icon ?? "😃"}</div>}
-      <div className="flex flex-col min-w-0">
-        {showUsername && (
-          <p>
-            <Username color={usernameColor}>
-              {username || "<unkown>"}
-              {isDeveloper && (
-                <Tooltip disableAnimation closeDelay={0} content="Developer">
-                  <Verified className="text-primary-600 max-h-[1.1rem] pb-0.5" />
-                </Tooltip>
-              )}
-            </Username>
-            <small className="text-default-500 pl-1">
-              {formatTimestamp(msg.data.timestamp)}
-            </small>
-          </p>
-        )}
-        <Markdown
-          className={clsx("text-wrap break-words", !showUsername && "pl-5")}
-          remarkPlugins={[remarkGfm, remarkBreaks, remarkGemoji]}
-          rehypePlugins={[[rehypeExternalLinks, { target: "_blank" }]]}
-          components={{
-            img(props) {
-              const { src, alt } = props;
-              return (
-                <>
-                  <img
-                    className="hover:cursor-pointer max-h-96 rounded-lg"
-                    src={src}
-                    alt={alt}
-                    onClick={() => {
-                      setImageModalSrc(src);
-                      imageOnOpen();
-                    }}
-                  ></img>
-                </>
-              );
-            },
-          }}
+      <div className="flex">
+        {showUsername && <div className="pr-2 w-5">{icon ?? "😃"}</div>}
+        <div
+          className={clsx("flex flex-col min-w-0", isEditing && "flex-grow")}
         >
-          {msg.data.content}
-        </Markdown>
+          {showUsername && (
+            <p>
+              <Username color={usernameColor}>
+                {username || "<unkown>"}
+                {isDeveloper && (
+                  <Tooltip disableAnimation closeDelay={0} content="Developer">
+                    <Verified className="text-primary-600 max-h-[1.1rem] pb-0.5" />
+                  </Tooltip>
+                )}
+              </Username>
+              <small className="text-default-500 pl-1">
+                {formatTimestamp(msg.data.timestamp)}
+              </small>
+            </p>
+          )}
+          {isEditing ? (
+            <Textarea
+              ref={textareaRef}
+              fullWidth
+              color="primary"
+              variant="faded"
+              size="md"
+              minRows={1}
+              maxRows={5}
+              maxLength={2069}
+              value={editedContent}
+              onValueChange={setEditedContent}
+              onKeyDown={handleKeyDown}
+            ></Textarea>
+          ) : (
+            <>
+              <Markdown
+                className={clsx(
+                  "text-wrap break-words",
+                  !showUsername && "pl-5",
+                )}
+                remarkPlugins={[remarkGfm, remarkBreaks, remarkGemoji]}
+                rehypePlugins={[[rehypeExternalLinks, { target: "_blank" }]]}
+                components={{
+                  img(props) {
+                    const { src, alt } = props;
+                    return (
+                      <>
+                        <img
+                          className="hover:cursor-pointer max-h-96 rounded-lg"
+                          src={src}
+                          alt={alt}
+                          onClick={() => {
+                            setImageModalSrc(src);
+                            imageOnOpen();
+                          }}
+                        ></img>
+                      </>
+                    );
+                  },
+                }}
+              >
+                {msg.data.content}
+              </Markdown>
+              {msg.edited && (
+                <p
+                  className={clsx(
+                    "text-default-500 text-xs",
+                    !showUsername && "pl-5",
+                  )}
+                >
+                  (edited)
+                </p>
+              )}
+            </>
+          )}
+        </div>
       </div>
+      {isHovering && (
+        <div className="absolute top-[-1rem] right-[1rem]">
+          <ButtonGroup size="sm">
+            {isOwned && (
+              <Button isIconOnly onPress={() => setIsEditing(!isEditing)}>
+                <Edit />
+              </Button>
+            )}
+          </ButtonGroup>
+        </div>
+      )}
     </div>
   );
 }
